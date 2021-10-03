b04ebb7eca29f3a6e100f905b19a0a544b6d828d
mineteeraa <patteera_mine@hotmail.com>
2021-10-03 17:09:25 +0700

    [3.12] Create Authenication part to login

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -41,6 +41,8 @@
 <script>
 import { Form, Field, ErrorMessage } from 'vee-validate'
 import * as yup from 'yup'
+import AuthService from '@/services/AuthService.js'
+
 export default {
   name: 'Login',
   components: {
@@ -57,6 +59,18 @@ export default {
       loading: false,
       message: '',
       schema
+    }
+  },
+  methods: {
+    handleLogin(user) {
+      AuthService.login(user)
+        .then(() => {
+          /* this.$router.go() */
+          this.$router.push({ name: 'EventList' })
+        })
+        .catch(() => {
+          this.message = 'could not login'
+        })
     }
   }
 }
